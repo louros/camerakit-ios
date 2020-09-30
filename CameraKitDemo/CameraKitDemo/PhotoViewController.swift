c8ac329e8a313ba653874c1c189a56f62107074d
Uros Lorencic <uloren@gmail.com>
2020-09-30 13:31:06 +0200

    add segmented controls for speed and timer

--- CameraKitDemo/CameraKitDemo/PhotoViewController.swift
+++ CameraKitDemo/CameraKitDemo/PhotoViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import CameraKit
 import AVFoundation
 import EasyPeasy
+import ScrollableSegmentedControl
 
 class PhotoPreviewViewController: UIViewController, UIScrollViewDelegate {
     
@@ -64,13 +65,6 @@ class PhotoSettingsViewController: UITableViewController {
         }
     }
     
-    @IBAction func handleFlash(_ sender: UISegmentedControl) {
-        if let session = self.previewView.session as? CKFPhotoSession {
-            let values: [CKFPhotoSession.FlashMode] = [.auto, .on, .off]
-            session.flashMode = values[sender.selectedSegmentIndex]
-        }
-    }
-    
     @IBAction func handleFace(_ sender: UISegmentedControl) {
         if let session = self.previewView.session as? CKFPhotoSession {
             session.cameraDetection = sender.selectedSegmentIndex == 0 ? .none : .faces
@@ -108,6 +102,68 @@ class PhotoViewController: UIViewController, CKFSessionDelegate {
     var progressTimer : Timer!
     var progress : CGFloat! = 0
     
+    lazy var speedSegmentedControl: ScrollableSegmentedControl = {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.layer.cornerRadius = 22
+        blurEffectView.clipsToBounds = true
+
+        let control = ScrollableSegmentedControl()
+        control.segmentStyle = .textOnly
+        control.insertSegment(withTitle: "0.3x", image: nil, at: 0)
+        control.insertSegment(withTitle: "0.5x", image: nil, at: 1)
+        control.insertSegment(withTitle: "1x", image: nil, at: 2)
+        control.insertSegment(withTitle: "2x", image: nil, at: 3)
+        control.insertSegment(withTitle: "3x", image: nil, at: 4)
+
+        control.underlineSelected = true
+        control.tintColor = .white
+        control.selectedSegmentContentColor = .white
+        control.fixedSegmentWidth = true
+        control.selectedSegmentIndex = 2
+        
+        control.insertSubview(blurEffectView, at: 0)
+        blurEffectView.easy.layout(Edges())
+        
+        return control
+    }()
+    
+    lazy var timerSegmentedControl: ScrollableSegmentedControl = {
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.layer.cornerRadius = 22
+        blurEffectView.clipsToBounds = true
+
+        let control = ScrollableSegmentedControl()
+        control.segmentStyle = .textOnly
+        control.insertSegment(withTitle: "5s", image: nil, at: 0)
+        control.insertSegment(withTitle: "10s", image: nil, at: 1)
+        control.insertSegment(withTitle: "15s", image: nil, at: 2)
+
+        control.underlineSelected = true
+        control.tintColor = .white
+        control.selectedSegmentContentColor = .white
+        control.fixedSegmentWidth = true
+        control.selectedSegmentIndex = 2
+        
+        control.insertSubview(blurEffectView, at: 0)
+        blurEffectView.easy.layout(Edges())
+        
+        return control
+    }()
+    
+    var closeButton: CameraOverlayButton!
+    var flipButton: CameraOverlayButton!
+    var filtersButton: CameraOverlayButton!
+    var timerButton: CameraOverlayButton!
+    var flashButton: CameraOverlayButton!
+    
+    var flashMode: CKFPhotoSession.FlashMode = .auto
+    var cameraType: CKFSession.CameraPosition = .back
+    
+    var isTimerControlOn: Bool = false
+    var isSpeedControlOn: Bool = false
+    
     func didChangeValue(session: CKFSession, value: Any, key: String) {
         if key == "zoom" {
             self.zoomLabel.text = String(format: "%.1fx", value as! Double)
@@ -143,23 +199,26 @@ class PhotoViewController: UIViewController, CKFSessionDelegate {
     }
     
     func setup() {
-        let closeButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "", icon: "Navigation/close")
+        closeButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "", icon: "Navigation/close")
         self.view.addSubview(closeButton)
         closeButton.easy.layout(Top(60), Left(10), Width(64), Height(64))
         
-        let flipButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Flip", icon: "Camera/flip")
+        flipButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Flip", icon: "Camera/flip")
+        flipButton.addTarget(self, action: #selector(flipCameraAction(_:)), for: .touchUpInside)
         self.view.addSubview(flipButton)
         flipButton.easy.layout(Top(60), Right(10), Width(64), Height(64))
         
-        let filtersButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Filters", icon: "Camera/filter")
+        filtersButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Filters", icon: "Camera/filter")
         self.view.addSubview(filtersButton)
         filtersButton.easy.layout(Top(20).to(flipButton), Right(10), Width(64), Height(64))
         
-        let timerButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Timer", icon: "Camera/timer")
+        timerButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Timer", icon: "Camera/timer")
+        timerButton.addTarget(self, action: #selector(timerSelectionAction(_:)), for: .touchUpInside)
         self.view.addSubview(timerButton)
         timerButton.easy.layout(Top(20).to(filtersButton), Right(10), Width(64), Height(64))
         
-        let flashButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Flash", icon: "Camera/flash-on")
+        flashButton = CameraOverlayButton(frame: CGRect(x: 0, y: 80, width: 64, height: 64), title: "Flash", icon: "Camera/flash-on")
+        flashButton.addTarget(self, action: #selector(toggleFlashAction(_:)), for: .touchUpInside)
         self.view.addSubview(flashButton)
         flashButton.easy.layout(Top(20).to(timerButton), Right(10), Width(64), Height(64))
         
@@ -206,6 +265,65 @@ class PhotoViewController: UIViewController, CKFSessionDelegate {
         UIView.transition(with: window, duration: 0.5, options: .transitionFlipFromLeft, animations: {
             window.rootViewController = vc
         }, completion: nil)
+    }
+    
+    @objc
+    func toggleFlashAction(_ sender: UIButton) {
+        switch flashMode {
+        case .on:
+            flashMode = .off
+            flashButton.icon = "Camera/flash-off"
+        case .off:
+            flashMode = .auto
+            flashButton.icon = "Camera/flash-auto"
+        case .auto:
+            flashMode = .on
+            flashButton.icon = "Camera/flash-on"
+        }
+        
+        print("flashMode \(flashMode.rawValue)")
+        if let session = self.previewView.session as? CKFPhotoSession {
+            session.flashMode = flashMode
+        }
+    }
+    
+    @objc
+    func timerSelectionAction(_ sender: UIButton) {
+        if !isTimerControlOn {
+            self.view.addSubview(timerSegmentedControl)
+            timerSegmentedControl.clipsToBounds = true
+            timerSegmentedControl.easy.layout(CenterX(), Bottom(20).to(recordButton), Width(180), Height(44))
+        }else {
+            timerSegmentedControl.removeFromSuperview()
+        }
+        isTimerControlOn = !isTimerControlOn
+    }
+    
+    @objc
+    func speedSelectionAction(_ sender: UIButton) {
+        if !isSpeedControlOn {
+            self.view.addSubview(speedSegmentedControl)
+            speedSegmentedControl.clipsToBounds = true
+            speedSegmentedControl.easy.layout(CenterX(), Bottom(20).to(recordButton), Width(280), Height(44))
+        }else {
+            speedSegmentedControl.removeFromSuperview()
+        }
+        isSpeedControlOn = !isSpeedControlOn
+    }
+    
+    @objc
+    func flipCameraAction(_ sender: UIButton) {
+        switch cameraType {
+        case .back:
+            cameraType = .front
+            
+        case .front:
+            cameraType = .back
+        }
+        
+        if let session = self.previewView.session as? CKFPhotoSession {
+            session.cameraPosition = cameraType
+        }
     }
     
     override var prefersStatusBarHidden: Bool {
